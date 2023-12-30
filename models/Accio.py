4b7b1ab7ffc3487820a9d11866f7dc871ddf8b42
Ahmed ata abdallah <72627749+Ahmed-ata112@users.noreply.github.com>
2023-12-30 23:20:50 +0200

    wtf

--- models/Accio.py
+++ models/Accio.py
@@ -1,8 +1,9 @@
 from torch import nn
 import torch
 
+
 class Accio(nn.Module):
-    def __init__(self, input_size,output_size, embedding_size=128, hidden_size=64, device=torch.device('cpu')):
+    def __init__(self, input_size, output_size, embedding_size=128, hidden_size=128, device=torch.device('cpu')):
         super().__init__()
         self.input_size = input_size
         self.hidden_size = hidden_size
@@ -12,14 +13,17 @@ class Accio(nn.Module):
         self.num_layers = 3
         self.device = device
         self.embedding = nn.Embedding(input_size, embedding_size)
-        self.rnn = nn.LSTM(embedding_size, hidden_size, batch_first=True, bidirectional=True, num_layers=3)
+        self.rnn = nn.LSTM(embedding_size, hidden_size,
+                           batch_first=True, bidirectional=True, num_layers=3)
         self.fc = nn.Linear(2*hidden_size, output_size)
         self.loss = nn.CrossEntropyLoss()
 
     def forward(self, x):
         batch_size = x.shape[0]
-        h0 = torch.zeros(self.bidirectional*self.num_layers, batch_size, self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.bidirectional*self.num_layers, batch_size, self.hidden_size).to(self.device)
+        h0 = torch.zeros(self.bidirectional*self.num_layers,
+                         batch_size, self.hidden_size).to(self.device)
+        c0 = torch.zeros(self.bidirectional*self.num_layers,
+                         batch_size, self.hidden_size).to(self.device)
         embeddings = self.embedding(x).to(self.device)
         h, (hn, cn) = self.rnn(embeddings, (h0, c0))
         # h is the output of the RNN
